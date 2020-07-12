24c0383e02aa5818e1f711d0329f1831836d7e59
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-07-12 11:07:55 +0100

    Remove strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -32,9 +32,7 @@ const persistor = persistStore(store)
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
+      <App />
     </PersistGate>
   </Provider>,
   document.getElementById('root')
