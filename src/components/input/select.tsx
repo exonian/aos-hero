b90c9f2fd801f5c0db79c573b51f39dc7d779f4c
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-07-17 17:11:30 +0100

    Remove unused type

--- src/components/input/select.tsx
+++ src/components/input/select.tsx
@@ -1,8 +1,6 @@
-import { ValueType, ActionMeta } from "react-select";
 import { titleCase } from "../../utils/text";
 
 export type TDropdownOption = { value: string; label: string }
-export type TSelectOneSetValueFn = (value: ValueType<TDropdownOption>, action: ActionMeta<any>) => void
 
 export const convertToOptions = (items: string[] = []): TDropdownOption[] => {
   return items.map(i => ({ value: i, label: titleCase(i) }))
