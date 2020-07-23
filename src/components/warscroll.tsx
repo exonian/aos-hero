2e912ea653e05d4f18afb303b7ca5ef06c39ab86
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-07-23 23:49:44 +0100

    Display warscroll stats

--- src/components/warscroll.tsx
+++ src/components/warscroll.tsx
@@ -9,15 +9,27 @@ export const WarscrollComponent: React.FC = () => {
   const ancestryKeywords = ancestry ? ancestry.keywords : []
   const archetypeKeywords = archetype ? archetype.keywords : []
   const combinedKeywords = ancestryKeywords.concat(armyKeywords, "HERO", archetypeKeywords)
+  const wounds = ancestry ? ancestry.wounds : null
+  const movement = ancestry ? ancestry.movement : null
+  const save = ancestry ? ancestry.save : null
+  const bravery = ancestry ? ancestry.bravery : null
   const cost = ancestry ? ancestry.cost : 0
 
   return (
     <div>
       <h2>{title} ({cost}DP)</h2>
       {ancestry ?
-        <p>
-          { combinedKeywords.join(', ') }
-        </p>
+        <>
+          <p>
+            { combinedKeywords.join(', ') }
+          </p>
+          <ul>
+            <li className="warscrollStat">WOUNDS: {wounds}</li>
+            <li className="warscrollStat">MOVEMENT: {movement}"</li>
+            <li className="warscrollStat">SAVE: {save}+</li>
+            <li className="warscrollStat">BRAVERY: {bravery}</li>
+          </ul>
+        </>
       : ''}
       <AbilitiesComponent addedAbilities={abilities} />
     </div>
