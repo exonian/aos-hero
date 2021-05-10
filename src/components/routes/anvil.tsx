82ba6d68c88db44f145064fe203c4492c9c6508f
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2021-05-10 21:55:38 +0100

    Reword tagline

--- src/components/routes/anvil.tsx
+++ src/components/routes/anvil.tsx
@@ -17,7 +17,7 @@ const Anvil: React.FC = () => {
         <h1>
           AoS Hero
         </h1>
-        <p>Sculpt your custom hero on the Anvil of Apotheosis</p>
+        <p>Forge your custom hero on the Anvil of Apotheosis</p>
         <p>Website by <a href="https://twitter.com/rogue_michael">Michael Blatherwick</a>, everything else from GHB 2020.</p>
       </div>
       <div className="row">
